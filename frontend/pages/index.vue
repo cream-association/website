7223287e2d6cf36ba641b1eaf27b450965be667d
Pixidream <developer@studiopixidream.com>
2024-03-26 22:36:53 +0100

    feat(frontend): new articles section on home page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
+import type { BlogPost } from "~/interfaces/blogPost";
+
 import { EnvelopeOpenIcon, CaretRightIcon } from "@radix-icons/vue";
 
 import Logo from "~/assets/images/logo.svg";
 import CFALogo from "~/assets/images/sponsors/cfa_insta_logo.png";
 
+const { isTabletOrMobile } = useDisplay();
+const runtimeConfig = useRuntimeConfig();
+const { locale } = useI18n();
+const blogPosts = ref<BlogPost[]>([]);
 const {
   data: blogPostResponse,
   error: blogPostError,
   pending: pendingBlogPost,
-} = useLazyFetch("/api/home-blog");
+  refresh: refreshBlogPost,
+} = useLazyFetch(() => "/api/home-blog");
+
+watch(blogPostResponse, (newResponse) => {
+  blogPosts.value = (newResponse?.items as unknown as BlogPost[]) || [];
+});
+
+onMounted(() => {
+  setTimeout(() => {
+    refreshBlogPost();
+  }, 200);
+});
 </script>
 
 <template>
@@ -62,6 +79,99 @@ const {
         </NuxtLinkLocale>
       </div>
     </section>
+    <section class="home__blog-posts" id="blog-posts">
+      <h2 class="home__blog-posts-title">
+        {{ $t("pages.home.blogTitle") }}
+      </h2>
+      <div class="navigation__left"></div>
+      <ClientOnly>
+        <Swiper
+          class="!pb-8"
+          :slides-per-view="'auto'"
+          :centered-slides="isTabletOrMobile()"
+          :space-between="20"
+          :pagination="{ clickable: true, dynamicBullets: true }"
+          :modules="[SwiperPagination, SwiperMousewheel]"
+          :mousewheel="!isTabletOrMobile()"
+        >
+          <SwiperSlide
+            v-for="i of 10"
+            v-if="pendingBlogPost"
+            :key="i"
+            class="!w-auto"
+          >
+            <Card class="home__blog-posts-card hoverable">
+              <Skeleton class="h-32 w-full rounded-xl" />
+              <CardHeader class="px-0">
+                <Skeleton class="h-4 w-60" />
+                <Skeleton class="h-4 w-56" />
+              </CardHeader>
+            </Card>
+          </SwiperSlide>
+          <SwiperSlide
+            v-else
+            v-for="blogPost in blogPosts"
+            :key="blogPost.id"
+            class="!w-auto"
+          >
+            <Card class="home__blog-posts-card">
+              <div class="home__blog-posts-card-image">
+                <Image
+                  :source="`${runtimeConfig.public.pocketBaseFileUrl}/${blogPost.collectionId}/${blogPost.id}/${blogPost.header_image}`"
+                  alt-text="post header image"
+                  class=""
+                />
+              </div>
+              <CardHeader class="home__blog-posts-card-header">
+                <div class="home__blog-posts-card-meta">
+                  <Avatar class="home__blog-posts-card-meta-avatar">
+                    <AvatarImage
+                      :src="`https://api.dicebear.com/8.x/bottts-neutral/svg?seed=${blogPost.author}`"
+                      alt="author avatar"
+                    />
+                    <AvatarFallback>{{
+                      blogPost.author
+                        .match(/(^\S\S?|\s\S)?/g)
+                        ?.map((v) => v.trim())
+                        .join("")
+                        .match(/(^\S|\S$)?/g)
+                        ?.join("")
+                        .toLocaleUpperCase()
+                    }}</AvatarFallback>
+                  </Avatar>
+                  <div class="home__blog-posts-card-meta-infos">
+                    <p class="home__blog-posts-card-author">
+                      {{ blogPost.author }}
+                    </p>
+                    <p class="home__blog-posts-card-updated">
+                      {{ $dayjs(blogPost.updated).format("LL") }}
+                    </p>
+                  </div>
+                </div>
+                <CardTitle class="home__blog-posts-card-title">
+                  {{ locale === "en" ? blogPost.title_en : blogPost.title_fr }}
+                </CardTitle>
+              </CardHeader>
+              <CardContent class="home__blog-posts-card-content">
+                <p>
+                  {{
+                    locale === "en"
+                      ? blogPost.description_en
+                      : blogPost.description_fr
+                  }}
+                </p>
+              </CardContent>
+              <CardFooter class="pt-4 px-4 pb-6">
+                <Button>
+                  Read More
+                  <CaretRightIcon class="h-4 w-4 ml-2" />
+                </Button>
+              </CardFooter>
+            </Card>
+          </SwiperSlide>
+        </Swiper>
+      </ClientOnly>
+    </section>
   </div>
 </template>
 
@@ -97,12 +207,42 @@ const {
   @apply py-8 mx-auto max-w-screen-xl px-4 lg:py-16;
 }
 .home__sponsors-title {
-  @apply mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-foreground md:text-4xl;
+  @apply mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-foreground;
 }
 .home__sponsors-wrapper {
   @apply grid grid-cols-2 gap-8 text-foreground sm:gap-12 md:grid-cols-3 lg:grid-cols-6;
 }
 .home__sponsors-item {
   @apply flex justify-center items-center;
+}
+.home__blog-posts {
+  @apply px-4 max-w-7xl w-full mx-auto mt-8;
+}
+.home__blog-posts-title {
+  @apply text-foreground font-bold text-3xl mb-8 text-center;
+}
+.home__blog-posts-card {
+  @apply min-h-48 w-72;
+}
+.home__blog-posts-card-header {
+  @apply px-4 pt-4 pb-2;
+}
+.home__blog-posts-card-image {
+  @apply inline-block overflow-hidden max-h-44 rounded-t-xl;
+}
+.home__blog-posts-card-meta {
+  @apply flex items-center gap-2 mb-4;
+}
+.home__blog-posts-card-meta-avatar {
+  @apply w-8 h-8;
+}
+.home__blog-posts-card-updated {
+  @apply text-xs text-muted-foreground;
+}
+.home__blog-posts-card-author {
+  @apply text-xs font-medium;
+}
+.home__blog-posts-card-content {
+  @apply break-all whitespace-break-spaces text-ellipsis line-clamp-4 px-4 pt-4 pb-8 max-h-28;
 }
 </style>
