d90c094c88e31e4ffcdd95203bc8f1ec72415721
Pixidream <developer@studiopixidream.com>
2024-03-26 16:23:50 +0100

    refactor(frontend): changed hero of home page.

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,21 +1,40 @@
 <script setup lang="ts">
 import Logo from "~/assets/images/logo.svg";
+import { EnvelopeOpenIcon } from "@radix-icons/vue";
+
+const {
+  data: blogPostResponse,
+  error: blogPostError,
+  pending: pendingBlogPost,
+} = useLazyFetch("/api/home-blog");
 </script>
 
 <template>
   <div class="home">
     <section class="home__header">
-      <Image
-        :source="Logo"
-        alt-text="cream association logo"
-        class="home__header-logo"
-      ></Image>
-      <div class="home__header-titles">
-        <h1 class="home__header-title">{{ $t("title") }}</h1>
-        <h2 class="home__header-subtitle">{{ $t("pages.home.header") }}</h2>
+      <div class="home__header-content">
+        <h1 class="home__header-content-title">{{ $t("title") }}</h1>
+        <p class="home__header-content-subtitle">
+          {{ $t("pages.home.header") }}
+        </p>
+        <div class="home__header-content-contact">
+          <Input
+            id="email"
+            type="email"
+            :placeholder="$t('pages.home.mailInput')"
+          />
+          <Button>
+            <EnvelopeOpenIcon class="w-4 h-4 mr-2" />
+            {{ $t("pages.home.contactBtn") }}
+          </Button>
+        </div>
       </div>
-      <div class="scroll hoverable">
-        <ScrollForMore />
+      <div class="home__header-logo">
+        <Image
+          :source="Logo"
+          alt-text="cream robotics logo gold color"
+          class="home__header-logo-image"
+        />
       </div>
     </section>
   </div>
@@ -23,30 +42,30 @@ import Logo from "~/assets/images/logo.svg";
 
 <style scoped>
 .home {
-  @apply min-h-full flex flex-col items-center justify-start;
+  @apply min-h-full flex flex-col;
 }
 
 .home__header {
-  @apply flex-col flex items-center xl:flex-row md:justify-center;
-  height: calc(100dvh - 2rem);
+  @apply rounded-md p-16 max-w-5xl grid mx-auto xl:gap-0 xl:w-4/5 lg:py-16 lg:gap-12 lg:grid-cols-12;
 }
 
+.home__header-content {
+  @apply order-1 place-self-center mb-4 mr-auto lg:-order-1 lg:mb-0 xl:col-span-8 lg:col-span-7;
+}
+
+.home__header-content-title {
+  @apply text-card-foreground text-center text-2xl tracking-tight font-extrabold max-w-2xl mb-4 xl:text-6xl md:text-5xl;
+}
+.home__header-content-subtitle {
+  @apply text-muted-foreground text-center mb-4 font-light max-w-2xl lg:text-xl lg:mb-8 md:text-lg;
+}
+.home__header-content-contact {
+  @apply items-center flex flex-col mb-3 gap-1.5 w-full max-w-2xl lg:flex-row lg:items-center;
+}
 .home__header-logo {
-  @apply aspect-auto h-72 w-2/4 xl:mb-4 xl:w-1/4;
+  @apply -order-1 max-w-sm place-self-center mb-4 xl:col-span-4 lg:order-1 lg:mb-0 lg:flex lg:mt-0 lg:col-span-5;
 }
-
-.home__header-titles {
-  @apply w-full text-center xl:text-left;
-}
-
-.home__header-title {
-  @apply text-6xl xl:text-9xl font-black tracking-widest uppercase;
-}
-.home__header-subtitle {
-  @apply text-lg xl:text-xl font-bold tracking-wide uppercase mt-4;
-}
-
-.scroll {
-  @apply relative xl:-left-2/4 xl:top-[35%] md:top-[20%] top-[10%] xl:-translate-x-2/4 cursor-pointer;
+.home__header-logo-image {
+  @apply max-w-full h-auto block;
 }
 </style>
