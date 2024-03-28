5d22cb308ee8e931b591bc9e9b086ec97282170b
pixidream <developer@studiopixidream.com>
2024-03-28 17:20:53 +0100

    Revert "fix(frontend): fix link width and replace a tag with NuxtLink"
    This reverts commit 15b6521b2f50980402212130684085fa6f685fb0.

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -6,19 +6,15 @@ const dayjs = useDayjs();
 
 <template>
   <div class="mx-auto max-w-screen-xl text-center">
-    <div
-      class="flex w-full justify-center items-center text-2xl font-semibold text-foreground"
+    <a
+      href="#"
+      class="flex justify-center items-center text-2xl font-semibold text-foreground hoverable"
     >
-      <NuxtLinkLocale
-        to="/"
-        class="hoverable w-fit flex justify-center items-center"
-      >
-        <div class="mr-2 mb-4 h-16">
-          <Image :source="Logo" alt-text="cream logo" />
-        </div>
-        Cream
-      </NuxtLinkLocale>
-    </div>
+      <div class="mr-2 mb-4 h-16">
+        <Image :source="Logo" alt-text="cream logo" />
+      </div>
+      Cream
+    </a>
     <ul class="flex flex-wrap justify-center items-center mb-6">
       <li>
         <NuxtLinkLocale to="/" class="mr-4 hover:underline md:mr-6 hoverable">
@@ -53,7 +49,7 @@ const dayjs = useDayjs();
     <div class="flex justify-center align-center flex-col gap-4">
       <span class="text-sm text-muted-foreground sm:text-center">
         © 2015-{{ dayjs().year() }}
-        <NuxtLinkLocale to="/" class="hover:underline">Cream</NuxtLinkLocale>
+        <a href="#" class="hover:underline">Cream</a>
         {{ $t("footer.rights") }}
       </span>
       <span class="text-sm text-muted-foreground sm:text-center">
