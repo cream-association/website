ac20bf854be20d13e8c750809bcd093fd9102e8a
pixidream <developer@studiopixidream.com>
2024-04-02 17:50:26 +0200

    feat(frontend): import prismjs as a composable

---
+++ frontend/composables/prism.ts
@@ -0,0 +1,12 @@
+import Prism from "prismjs";
+import "prismjs/themes/prism-twilight.css";
+import "prismjs/components/prism-rust";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-nginx";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-yaml";
+import "prismjs/components/prism-json";
+
+export default Prism;
