644388690e084d3ff43e4cedacd4a485b5f282ff
pixidream <developer@studiopixidream.com>
2024-04-11 16:13:05 +0200

    feat(frontend): add a preview of gallery on homepage

--- frontend/assets/locales/_fr/pages/home.ts
+++ frontend/assets/locales/_fr/pages/home.ts
@@ -1,6 +1,7 @@
 export default {
   header: "Partageons le frisson de la compétition et la joie de l'innovation.",
   blogTitle: "Les derniers articles",
+  galleryTitle: "Les dernières photos",
   contactBtn: "Contactez nous !",
   mailInput: "Votre adresse e-mail",
   sponsorsTitle: "Ils nous font confiance",
