bac7b7aa657391b473d0e8deb0c1ed6b8e97d7f6
pixidream <developer@studiopixidream.com>
2024-04-11 17:16:17 +0200

    fix(frontend): remove debug log

--- frontend/plugins/vercel.client.ts
+++ frontend/plugins/vercel.client.ts
@@ -4,7 +4,6 @@ import { useStorage } from "@vueuse/core";
 export default defineNuxtPlugin(() => {
   const allowCookies = useStorage("allow-cookies", false);
   if (process.client && allowCookies.value) {
-    console.log("Allow cookies, injecting...");
     inject();
   }
 });
