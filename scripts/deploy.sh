7d73c88c65901cc06d8c4a4514015ba3be26e37b
pixidream <developer@studiopixidream.com>
2024-04-22 10:35:32 +0200

    feat: add deployment script and workflow

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$(whoami)" = "pixi" ]; then
+    cd "$HOME/creamrobotics"
+    git pull
+    docker-compose down && docker-compose up --build -d
+else
+    echo "This script must be run as user 'pixi'."
+fi
